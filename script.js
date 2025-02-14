93ca996d78cca650d3108f84e0e2d24682596b4b
gleidson <gleidsonfelix220@gmail.com>
2025-02-14 12:10:06 -0300

    adicionando funcionalidade para capturar o nome da tarefa e exibila no documento

---
+++ script.js
@@ -0,0 +1,35 @@
+const addNewTask = document.querySelector("#addTaskBtn");
+const newTask = document.querySelector("#taskinput");
+const tasksContainer = document.querySelector(".task-container");
+
+
+addNewTask.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    if (newTask.value.trim() === "") {
+
+        alert("Por favor, insira uma tarefa");
+
+        return;
+
+    } else {
+
+        const task = document.createElement("div");
+        task.classList.add("task");
+        const taskName = document.createElement("p");
+        taskName.innerText = newTask.value;
+    
+        const options = document.createElement("div");
+    
+        task.appendChild(taskName);
+        tasksContainer.appendChild(task)
+
+    }
+
+
+    newTask.value = ""
+
+
+
+})
