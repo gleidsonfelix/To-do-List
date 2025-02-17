7b51994f9ce8b1f8025871dc7053fc069077da08
gleidson <gleidsonfelix220@gmail.com>
2025-02-17 14:19:25 -0300

    adicionando botões de edição e remoção de tarefas pelo DOM

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ const tasksContainer = document.querySelector(".task-container");
 
 addNewTask.addEventListener("click", (e) => {
 
+    /*prevent default*/
+
     e.preventDefault();
+
+    /*verificação de input vazio*/
 
     if (newTask.value.trim() === "") {
 
@@ -13,16 +17,31 @@ addNewTask.addEventListener("click", (e) => {
 
         return;
 
+    /*começo da inserção das tarefas*/    
+
     } else {
 
         const task = document.createElement("div");
         task.classList.add("task");
         const taskName = document.createElement("p");
         taskName.innerText = newTask.value;
-    
+
+        const editTaskIcon = document.createElement("ion-icon");
+        editTaskIcon.setAttribute("name", "create-outline");
+        editTaskIcon.setAttribute("id", "editTaskBtn");
+
+        const removeTaskIcon = document.createElement("ion-icon");
+        removeTaskIcon.setAttribute("name", "trash-outline");
+        removeTaskIcon.setAttribute("id", "removeTaskBtn");
+
         const options = document.createElement("div");
+        options.classList.add("options");
+
+        options.appendChild(editTaskIcon);
+        options.appendChild(removeTaskIcon);
     
         task.appendChild(taskName);
+        task.appendChild(options);
         tasksContainer.appendChild(task);
 
     }
